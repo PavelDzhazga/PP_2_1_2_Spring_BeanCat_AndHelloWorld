83f54245b907784b2a13efff926a460642d7fa54
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-10-06 20:49:09 +0300

    Ok!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(beanCat1.equals(beanCat2));
     }
 }
